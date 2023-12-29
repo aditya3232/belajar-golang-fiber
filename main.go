d66fe1ecdcc6094b4d133e57a6ac93236ad0e4ba
Muhammad Aditya <81363093+aditya3232@users.noreply.github.com>
2023-12-29 16:53:48 +0700

    configuration - menambahkan config timeout pada request

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		IdleTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second, // diatas 5 detik write  maka timeout, stop requestnya
+		ReadTimeout:  5 * time.Second,
+	})
 
 	err := app.Listen("localhost:3000")
 	if err != nil {
