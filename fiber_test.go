3e34be6af62c7f089272825a3351a322f2076368
Muhammad Aditya <81363093+aditya3232@users.noreply.github.com>
2024-01-02 10:02:32 +0700

    contoh ctx query

--- fiber_test.go
+++ fiber_test.go
@@ -9,8 +9,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	app = fiber.New()
+)
+
 func TestRoutingHelloWorld(t *testing.T) {
-	app := fiber.New()
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Hello World")
@@ -25,4 +28,22 @@ func TestRoutingHelloWorld(t *testing.T) {
 	bytes, err := io.ReadAll(response.Body)
 	assert.Nil(t, err)
 	assert.Equal(t, "Hello World", string(bytes))
+}
+
+func TestCtx(t *testing.T) {
+
+	app.Get("/hello", func(ctx *fiber.Ctx) error {
+		name := ctx.Query("name", "Guest")
+		return ctx.SendString("Hello " + name)
+	})
+
+	request := httptest.NewRequest("GET", "/hello?name=Adit", nil)
+	response, err := app.Test(request)
+
+	assert.Nil(t, err)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Adit", string(bytes))
+
 }
